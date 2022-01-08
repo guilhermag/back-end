59169987a468956323e31969b1af5705be69f98e
guilherme gabriel <guilhermag@gmail.com>
2022-01-08 15:57:54 -0300

    adicionado as tratativas de erros para o controller.js

--- routes/controller.js
+++ routes/controller.js
@@ -2,108 +2,143 @@ const pool = require('../database');
 const queries = require('./queries');
 
 
-const getCandidatos = (req, res) => {
-    pool.query(queries.getCandidatos, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
+const getCandidatos = async (req, res) => {
+    try {
+        await pool.query(queries.getCandidatos, (error, results) => {
+            if (error) throw error;
+            res.status(200).json(results.rows);
+        });
+    } catch (err) {
+        return res.status(400).send("Erro na consulta de candidatos" + err);
+    }
+
 };
 
-const getCandidatoByEmail = (req, res) => {
-    const email = req.params.email;
-    pool.query(queries.getCandidatoByEmail, [email], (error, results) =>{
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    })
+const getCandidatoByEmail = async (req, res) => {
+    try {
+        const email = req.params.email;
+        await pool.query(queries.getCandidatoByEmail, [email], (error, results) => {
+            if (error) throw error;
+            res.status(200).json(results.rows);
+        });
+    } catch (err) {
+        return res.status(400).send("" + err);
+    }
+
 }
 
-const createCandidato = (req, res) => {
-    const { email, nome, sobrenome, data_nascimento, cpf } = req.body;
-    // verificar se o email existe e os campos não são nulos
-    let checkCandidatoVazio =
-        (email === "") ||
-        (nome === "") ||
-        (sobrenome === "") ||
-        (data_nascimento === "") ||
-        (cpf === "");
+const createCandidato = async (req, res) => {
+    try {
+        const {email, nome, sobrenome, data_nascimento, cpf} = req.body;
+        // verificar se o email existe e os campos não são nulos
+        let checkCandidatoVazio =
+            (email === "") ||
+            (nome === "") ||
+            (sobrenome === "") ||
+            (data_nascimento === "") ||
+            (cpf === "");
 
-    let checkCandidatoUndefined =
-        (email === undefined) ||
-        (nome === undefined) ||
-        (sobrenome === undefined) ||
-        (data_nascimento === undefined) ||
-        (cpf === undefined);
+        let checkCandidatoUndefined =
+            (email === undefined) ||
+            (nome === undefined) ||
+            (sobrenome === undefined) ||
+            (data_nascimento === undefined) ||
+            (cpf === undefined);
 
-    let dataNascimento = new Date(data_nascimento);
-    let diferencaDatas = Date.now() - dataNascimento.getTime();
-    let idadeData = new Date(diferencaDatas);
-    let idadeCalculada = Math.abs(idadeData.getUTCFullYear() - 1970);
+        let dataNascimento = new Date(data_nascimento);
+        let diferencaDatas = Date.now() - dataNascimento.getTime();
+        let idadeData = new Date(diferencaDatas);
+        let idadeCalculada = Math.abs(idadeData.getUTCFullYear() - 1970);
 
-    pool.query(queries.checkEmailExists, [email], (error, results) => {
-        if (results.rows.length) {
-            throw new Error("Cadastro não efetuado, email já existe");
-        }
-        if (checkCandidatoVazio || checkCandidatoUndefined) {
-            throw new Error("Cadastro não efetuado, possui campo nulo.");
-        }
-        if (idadeCalculada < 16) {
-            throw new Error("Cadastro não efetuado, idade menor que 16 anos.");
-        }
+        await pool.query(queries.checkEmailExists, [email], (error, results) => {
+            if (results.rows.length) {
+                throw new Error("Cadastro não efetuado, email já existe");
+            }
+            if (checkCandidatoVazio || checkCandidatoUndefined) {
+                throw new Error("Cadastro não efetuado, possui campo nulo.");
+            }
+            if (idadeCalculada < 16) {
+                throw new Error("Cadastro não efetuado, idade menor que 16 anos.");
+            }
 
-        pool.query(queries.createCandidato,
-            [email, nome, sobrenome, data_nascimento, cpf], (error, results) => {
-            if (error) throw error;
-            res.status(201).send("Candidato cadastrado com sucesso");
+            pool.query(queries.createCandidato,
+                [email, nome, sobrenome, data_nascimento, cpf], (error, results) => {
+                    if (error) throw error;
+                    res.status(201).send("Candidato cadastrado com sucesso");
+                })
+        });
+
+    } catch (err) {
+        return res.status(400).send("" + err);
+    }
+
+};
+
+const deleteCandidato = async (req, res) => {
+    try {
+        const email = req.params.email;
+
+        await pool.query(queries.getCandidatoByEmail, [email], (error, results) => {
+            const candidatoNaoEncontrado = !results.rows.length;
+            if (candidatoNaoEncontrado) {
+                res.send('Candidato não existe no banco de dados.');
+
+            }
+
+            pool.query(queries.deleteCandidato, [email], (error, results) => {
+                if (error) throw error;
+                res.status(200).send("Candidato removido com sucesso.")
             })
-    });
+        });
+
+    } catch (err) {
+        return res.status(400).send("Erro ao deletar candidato." + err);
+    }
+
 };
 
-const deleteCandidato = (req, res) => {
-    const email = req.params.email;
+const atualizarCandidato = async (req, res) => {
+    try {
+        const email = req.params.email;
+        let {nome, sobrenome, data_nascimento, cpf} = req.body;
 
-    pool.query(queries.getCandidatoByEmail, [email], (error, results) => {
-        const candidatoNaoEncontrado = !results.rows.length;
-        if (candidatoNaoEncontrado) {
-            res.send('Candidato não existe no banco de dados.');
-        }
+        await pool.query(queries.getCandidatoByEmail, [email], (error, results) => {
+            //console.log(results.rows[0].nome);
+            const candidatoNaoEncontrado = !results.rows.length;
+            if (candidatoNaoEncontrado) {
+                res.send('Candidato não existe no banco de dados.');
+            }
 
-        pool.query(queries.deleteCandidato, [email], (error, results) => {
-            if (error) throw error;
-            res.status(200).send("Candidato removido com sucesso.")
-        })
-    });
-};
+            if (req.body.nome === undefined) {
+                nome = results.rows[0].nome
+            }
+            if (req.body.sobrenome === undefined) {
+                sobrenome = results.rows[0].sobrenome
+            }
+            if (req.body.data_nascimento === undefined) {
+                data_nascimento = results.rows[0].data_nascimento
+            }
+            if (req.body.cpf === undefined) {
+                cpf = results.rows[0].cpf
+            }
 
-const atualizarCandidato = (req, res) => {
-    const email = req.params.email;
-    let { nome, sobrenome, data_nascimento, cpf } = req.body;
+            let hoje = new Date();
+            let dataAtualizacao = hoje.getFullYear() + '-' + (hoje.getMonth() + 1) + '-' + hoje.getDate();
+            let horaAtualizacao = hoje.getHours() + ':' + hoje.getMinutes() + ':' + hoje.getSeconds();
+            let atualizacaoExata = dataAtualizacao + ' ' + horaAtualizacao;
 
-    pool.query(queries.getCandidatoByEmail, [email], (error, results) => {
-        //console.log(results.rows[0].nome);
-        const candidatoNaoEncontrado = !results.rows.length;
-        if (candidatoNaoEncontrado) {
-            res.send('Candidato não existe no banco de dados.');
-        }
+            pool.query(queries.atualizarCandidato,
+                [nome, sobrenome, data_nascimento, cpf, atualizacaoExata, email], (error, results) => {
+                    if (error) throw error;
+                    res.status(200).send('Candidato foi atualizado com sucesso.');
+                });
+        });
 
-        if (req.body.nome === undefined) {nome = results.rows[0].nome}
-        if (req.body.sobrenome === undefined) {sobrenome = results.rows[0].sobrenome}
-        if (req.body.data_nascimento === undefined) {data_nascimento = results.rows[0].data_nascimento}
-        if (req.body.cpf === undefined) {cpf = results.rows[0].cpf}
+    } catch (err) {
+        return res.status(400).send("Erro ao atualizar candidato." + err);
+    }
 
-        let hoje = new Date();
-        let dataAtualizacao = hoje.getFullYear()+'-'+(hoje.getMonth()+1)+'-'+hoje.getDate();
-        let horaAtualizacao = hoje.getHours() + ':' + hoje.getMinutes() + ':' + hoje.getSeconds();
-        let atualizacaoExata = dataAtualizacao+' '+horaAtualizacao;
-
-        pool.query(queries.atualizarCandidato,
-            [nome, sobrenome, data_nascimento, cpf, atualizacaoExata, email], (error, results) => {
-            if (error) throw error;
-            res.status(200).send('Candidato foi atualizado com sucesso.');
-            });
-    })
 }
-
-
 
 module.exports = {
     getCandidatos,
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const rotasCandidatos = require('./routes/routes');
 const { Pool } = require('pg')
 
 const pool = require('./database');
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world!');
