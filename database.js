2e2f7a6de8935432c7abdc3a74fe2f3c85e2fb8c
guilherme gabriel <guilhermag@gmail.com>
2022-01-08 16:08:25 -0300

    teste heroku

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 const Pool = require('pg').Pool;
-require('dotenv').config()
+//require('dotenv').config()
 
  const pool = new Pool({
-     connectionString: process.env.POSTGRES_URL
+     //connectionString: process.env.POSTGRES_URL
+     connectionString: "postgres://bephbdbz:25hcAV96-gcDhEhcrrcZAZGv_gPvSYpa@motty.db.elephantsql.com/bephbdbz"
+
  });
 
 
